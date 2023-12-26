9ae01ac236e4e785429d6c7ff559718edeb5d8ad
Daniel Albano III <captdane3@gmail.com>
2023-12-27 00:58:16 +0800

    Redesigned and simplified

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -1,291 +1,363 @@
 using Godot;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
-public class Main : Node2D
+public class Main : Control
 {
-    private Node2D Cells;
-    private Godot.Collections.Array CellsArray;
-    private int TurnCount;
-    private bool IsX;
-    private bool IsGameOver;
-    private Label DeclareLabel;
-    private Label GameOverLabel;
-    private TextureRect Slasher;
-    private TextureRect TurnSign;
-    private Button ReloadBtn;
-    private Button QuitBtn;
-    private Button NextBtn;
-    private Texture Texture;
-    private bool PlayerTurn;
-    private float _delay;
+    private Label ResultLabel;
+
+    private string PlayerMark;
+    private string OpponentMark;
+    private bool PlayersTurn;
+    private bool IsEndRound;
+
+    private CanvasLayer Overlay;
+    private TextureButton XBtn;
+    private TextureButton OBtn;
+    private Button ExitBtn;
+
+    private Control Cells;
+    private List<TextureButton> CellList;
+
+    private Texture texture;
+    private string[] marksArray;
+    private float Delay;
+
+    private int PlayerScore;
+    private int OpponentScore;
+
+    private Label PlayerScoreLabel;
+    private Label OpponentScoreLabel;
+
+    private TextureRect Bar1;
+    private TextureRect Bar2;
 
     public override void _Ready()
     {
-        base._Ready();
-        _delay = 1.0f;
-        PlayerTurn = true;
-        IsGameOver = false;
-        ReloadBtn = GetNode<Button>("Control/Buttons/Reload");
-        QuitBtn = GetNode<Button>("Control/Buttons/Quit");
-        NextBtn = GetNode<Button>("Control/Buttons/Next");
-        Slasher = GetNode<TextureRect>("Slasher");
-        TurnSign = GetNode<TextureRect>("Control/TurnSign");
-        DeclareLabel = GetNode<Label>("Control/Labels/DeclareLabel");
-        GameOverLabel = GetNode<Label>("Control/Labels/GameOverLabel");
-        Texture = ResourceLoader.Load<Texture>("res://Assets/marks.png");
-        TurnCount = 0;
-        Cells = GetNode<Node2D>("Cells");
-        CellsArray = Cells.GetChildren();
-        Slasher.Visible = false;
-        SetGrid();
-        ReloadBtn.Connect("pressed", this, "ReloadScene");
-        QuitBtn.Connect("pressed", this, "QuitGame");
-    }
+        Bar1 = GetNode<TextureRect>("SlasherControl/Bar1");
+        Bar2 = GetNode<TextureRect>("SlasherControl/Bar2");
 
-    private void UpdateTurn()
-    {
-        if (IsGameOver) return;
-        float pos;
-        //updates image on left side sign to show which is the current turn x or o
-        AtlasTexture img = new AtlasTexture();
-        img.Atlas = Texture;
-        pos = IsX ? 200f : 100f;
-        img.Region = new Rect2(pos, 0f, 100f, 100f);
-        TurnSign.Texture = img;
+        ResultLabel = GetNode<Label>("ResultLabel");
+        PlayerScoreLabel = GetNode<Label>("ScoreboardBox/VsBox/PlayerBox/PlayerScore");
+        OpponentScoreLabel = GetNode<Label>("ScoreboardBox/VsBox/OpponentBox/OpponentScore");
 
-        IsX = TurnCount % 2 == 0;
-        Cell.IsPlayerTurn = IsX;
-        if (!IsX)
+        Delay = 0.4f;
+        marksArray = new string[9];
+        texture = ResourceLoader.Load<Texture>("res://Assets/marks.png");
+        Overlay = GetNode<CanvasLayer>("Overlay");
+        XBtn = GetNode<TextureButton>("Overlay/MarkerChoices/Choices/X");
+        OBtn = GetNode<TextureButton>("Overlay/MarkerChoices/Choices/O");
+        ExitBtn = GetNode<Button>("Overlay/ExitBtn");
+
+        ExitBtn.Connect("pressed", this, "ExitGame");
+        XBtn.Connect("pressed", this, "SetPlayersMarkChoice", new Godot.Collections.Array {"x"});
+        OBtn.Connect("pressed", this, "SetPlayersMarkChoice", new Godot.Collections.Array {"o"});
+
+        Cells = GetNode<Control>("GridControl/Cells");
+        Godot.Collections.Array cellsArr = Cells.GetChildren();
+        CellList = new List<TextureButton>();
+        foreach (TextureButton cell in cellsArr)
         {
-            PCPlayCell();
+            CellList.Add(cell);
+            cell.Connect("pressed", this, "ClickCell", new Godot.Collections.Array {cell});
         }
+
+        PlayerScore = 0;
+        OpponentScore = 0;
+
+        Overlay.Visible = true;
+        Bar1.Visible = false;
+        Bar2.Visible = false;
     }
 
-    private void QuitGame()
+    private void SetPlayersMarkChoice(string choice)
+    {
+        if (choice == "x")
+        {
+            PlayerMark = "x";
+            OpponentMark = "o";
+            PlayersTurn = true;
+        }
+        else 
+        {
+            PlayerMark = "o";
+            OpponentMark = "x";
+            PlayersTurn = false;
+        }
+        StartGame();
+    }
+
+    private void ExitGame()
     {
         GetTree().Quit();
     }
 
-    private void ReloadScene()
+    private void StartGame()
     {
-        GetTree().ReloadCurrentScene();
-    }
-
-    private void SetGrid()
-    {
-        int count = 0;
-        float size = 120f;
-        for (int row = 0; row < 3; row++)
+        Overlay.Hide();
+        if (!PlayersTurn)
         {
-            for (int col = 0; col < 3; col++)
-            {
-                if (CellsArray[count] is Cell _cell)
-                {
-                    _cell.Position = new Vector2(col * size,row * size);
-                    _cell.SetRow(row);
-                    _cell.SetCol(col);
-                    _cell.Connect("ClickSignal", this, "OnCellClicked");
-                }
-                count++;
-            }
+            PCPlay();
         }
     }
 
-    private void OnCellClicked(Cell _cell)
+    private bool IsMarksArrayFull()
     {
-        if (IsGameOver)
+        foreach (object cell in marksArray)
         {
-            return;
+            if (cell == null) return false;
+        }
+        return true;
+    }
+
+    private async void PCPlay()
+    {
+        bool searching = true;
+        TextureButton choice;
+        while (searching && !IsMarksArrayFull())
+        {
+            Random random = new Random();
+            int i = random.Next(9);
+            if (marksArray[i] == null)
+            {
+                choice = CellList[i];
+                await ToSignal(GetTree().CreateTimer(Delay), "timeout");
+                MarkCell(false, choice);
+                searching = false;
+            }
+        }
+        PlayersTurn = true;
+    }
+
+    private void ClickCell(TextureButton cell)
+    {
+        if (PlayersTurn)
+        {
+            MarkCell(true, cell);
+            PlayersTurn = false;
+            PCPlay();
+        }
+    }
+
+    private void MarkCell(bool isPlayer, TextureButton cell)
+    {
+        if (IsEndRound) return;
+
+        AtlasTexture img = new AtlasTexture();
+        img.Atlas = texture;
+        string mark = "";
+
+        if (isPlayer)
+        {
+            if (PlayerMark == "x")
+            {
+                img.Region = new Rect2(0f,100f,100f,100f);
+                mark = "x";
+            }
+            else
+            {
+                img.Region = new Rect2(100f,100f,100f,100f);
+                mark = "o";
+            }
         }
         else
         {
-            IsX = TurnCount % 2 == 0;
-            Cell.IsPlayerTurn = IsX;
-            _cell.MarkCell(IsX);
-            CheckForWinner();
-            TurnCount++;
-            
-            UpdateTurn();
+            if (OpponentMark == "x")
+            {
+                img.Region = new Rect2(0f,100f,100f,100f);
+                mark = "x";
+            }
+            else
+            {
+                img.Region = new Rect2(100f,100f,100f,100f);
+                mark = "o";
+            }
         }
+
+        cell.TextureNormal = img;
+        cell.Disabled = true;
+        RecordMarks(cell, mark);
+        CheckForWinner();
     }
 
-    public override void _Input(InputEvent @event)
+    private void RecordMarks(TextureButton cell, string mark)
     {
-        base._Input(@event);
-        foreach (Cell _cell in CellsArray)
-        {
-            _cell._CellInput(@event);
-        }
+        int numName = cell.GetPositionInParent();
+        marksArray[numName] = mark;   
     }
 
     private void CheckForWinner()
     {
-        
-        int patInt = -1;
-        for (int i = 0; i < 8; i++)
+        string pattern = "";
+        for (int i = 0; i < 10; i++)
         {
-            string pattern = "";
-            switch (i)
+            switch(i)
             {
                 case 0:
-                    pattern = ((Cell)CellsArray[0]).marker + ((Cell)CellsArray[1]).marker + ((Cell)CellsArray[2]).marker;
-                    patInt = i;
+                    pattern = marksArray[0] + marksArray[1] + marksArray[2];
                     break;
                 case 1:
-                    pattern = ((Cell)CellsArray[3]).marker + ((Cell)CellsArray[4]).marker + ((Cell)CellsArray[5]).marker;
-                    patInt = i;
+                    pattern = marksArray[3] + marksArray[4] + marksArray[5];
                     break;
                 case 2:
-                    pattern = ((Cell)CellsArray[6]).marker + ((Cell)CellsArray[7]).marker + ((Cell)CellsArray[8]).marker;
-                    patInt = i;
+                    pattern = marksArray[6] + marksArray[7] + marksArray[8];
                     break;
                 case 3:
-                    pattern = ((Cell)CellsArray[0]).marker + ((Cell)CellsArray[3]).marker + ((Cell)CellsArray[6]).marker;
-                    patInt = i;
+                    pattern = marksArray[0] + marksArray[3] + marksArray[6];
                     break;
                 case 4:
-                    pattern = ((Cell)CellsArray[1]).marker + ((Cell)CellsArray[4]).marker + ((Cell)CellsArray[7]).marker;
-                    patInt = i;
+                    pattern = marksArray[1] + marksArray[4] + marksArray[7];
                     break;
                 case 5:
-                    pattern = ((Cell)CellsArray[2]).marker + ((Cell)CellsArray[5]).marker + ((Cell)CellsArray[8]).marker;
-                    patInt = i;
+                    pattern = marksArray[2] + marksArray[5] + marksArray[8];
                     break;
                 case 6:
-                    pattern = ((Cell)CellsArray[0]).marker + ((Cell)CellsArray[4]).marker + ((Cell)CellsArray[8]).marker;
-                    patInt = i;
+                    pattern = marksArray[0] + marksArray[4] + marksArray[8];
                     break;
                 case 7:
-                    pattern = ((Cell)CellsArray[2]).marker + ((Cell)CellsArray[4]).marker + ((Cell)CellsArray[6]).marker;
-                    patInt = i;
+                    pattern = marksArray[2] + marksArray[4] + marksArray[6];
+                    break;
+                
+                case 8:
+                    pattern = marksArray[1] + marksArray[4] + marksArray[7] + marksArray[3] + marksArray[5];
+                    break;
+                case 9:
+                    pattern = marksArray[0] + marksArray[4] + marksArray[8] + marksArray[2] + marksArray[6];
                     break;
             }
-
             if (pattern == "xxx")
             {
-                DeclareWinner("x");
-                DrawSlash(patInt);
+                DeclareWinner("x", i);
             }
             else if (pattern == "ooo")
             {
-                DeclareWinner("o");
-                DrawSlash(patInt);
+                DeclareWinner("o", i);
+            }
+            else if (pattern == "xxxxx")
+            {
+                DeclareWinner("x", i);
+            }
+            else if (pattern == "ooooo")
+            {
+                DeclareWinner("o", i);
             }
             else
             {
-                if (!IsGameOver)
+                if (!IsEndRound)
                 {
-                    CheckDraw();
+                    CheckForDraw();
                 }
             }
         }
     }
 
-    private void DeclareWinner(string winner)
+    private void DeclareWinner(string winner, int casePattern)
     {
-        DeclareLabel.Text = winner + " wins!";
-        DeclareGameOver();
+        if (winner == PlayerMark)
+        {
+            ResultLabel.Text = "You Win!";
+            AddPointPlayer();
+        }
+        else
+        {
+            ResultLabel.Text = "You Lose!";
+            AddPointOpponent();
+        }
+        ResultLabel.Show();
+        DrawSlash(casePattern);
+        IsEndRound = true;
     }
 
-    private void CheckDraw()
+    private void CheckForDraw()
     {
-        foreach (Cell _cell in CellsArray)
+        if (IsMarksArrayFull())
         {
-            if (_cell.marker == "")
-            {
-                return;
-            }
+            DeclareDraw();
         }
-        DeclareDraw();
+        else
+        {
+            return;
+        }
     }
 
     private void DeclareDraw()
     {
-        DeclareLabel.Text = "It's a draw!";
-        DeclareGameOver();
+        ResultLabel.Text = "It's a Draw!";
+        ResultLabel.Show();
     }
 
-    private void DeclareGameOver()
+    private void AddPointPlayer()
     {
-        IsGameOver = true;
-        GameOverLabel.Text = "GAME OVER!";
+        PlayerScore++;
+        UpdateScore();
+    }
+
+    private void AddPointOpponent()
+    {
+        OpponentScore++;
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        PlayerScoreLabel.Text = PlayerScore.ToString().PadLeft(3, '0');
+        OpponentScoreLabel.Text = OpponentScore.ToString().PadLeft(3, '0');
     }
 
     private void DrawSlash(int pattern)
     {
-        Slasher.Visible = true;
-
-        switch (pattern)
+        Bar1.Visible = true;
+        switch(pattern)
         {
-            case -1:
-                break;
-
             case 0:
-                Slasher.SetRotation(0f);
-                Slasher.SetPosition(new Vector2(348f,168f));
+                Bar1.SetPosition(new Vector2(416f,148f));
+                Bar1.SetRotation(0f);
                 break;
             case 1:
-                Slasher.SetRotation(0f);
-                Slasher.SetPosition(new Vector2(348f,280f));
+                Bar1.SetPosition(new Vector2(416f,264f));
+                Bar1.SetRotation(0f);
                 break;
             case 2:
-                Slasher.SetRotation(0f);
-                Slasher.SetPosition(new Vector2(348f,404f));
+                Bar1.SetPosition(new Vector2(416f,384f));
+                Bar1.SetRotation(0f);
                 break;
-
 
             case 3:
-                Slasher.SetRotation((float)Math.PI * 0.5f);
-                Slasher.SetPosition(new Vector2(412f,120f));
+                Bar1.SetPosition(new Vector2(560f,28f));
+                Bar1.SetRotation((float)(Math.PI / 2));
                 break;
             case 4:
-                Slasher.SetRotation((float)Math.PI * 0.5f);
-                Slasher.SetPosition(new Vector2(528f,120f));
+                Bar1.SetPosition(new Vector2(672f,28f));
+                Bar1.SetRotation((float)(Math.PI / 2));
                 break;
             case 5:
-                Slasher.SetRotation((float)Math.PI * 0.5f);
-                Slasher.SetPosition(new Vector2(644f,120f));
+                Bar1.SetPosition(new Vector2(800f,28f));
+                Bar1.SetRotation((float)(Math.PI / 2));
                 break;
 
             case 6:
-                Slasher.SetRotation((float)Math.PI * 0.25f);
-                Slasher.SetPosition(new Vector2(404f,160f));
+                Bar1.SetPosition(new Vector2(496f,92f));
+                Bar1.SetRotation((float)(Math.PI / 4));
                 break;
             case 7:
-                Slasher.SetRotation((float)Math.PI * -0.258f);
-                Slasher.SetPosition(new Vector2(400f,410f));
+                Bar1.SetPosition(new Vector2(852f,92f));
+                Bar1.SetRotation((float)(3 * Math.PI / 4));
+                break;
+
+            case 8:
+                Bar1.SetPosition(new Vector2(672f,28f));
+                Bar1.SetRotation((float)(Math.PI / 2));
+                Bar2.Visible = true;
+                Bar2.SetPosition(new Vector2(416f,264f));
+                Bar2.SetRotation(0f);
+                break;
+            case 9:
+                Bar1.SetPosition(new Vector2(496f,92f));
+                Bar1.SetRotation((float)(Math.PI / 4));
+                Bar2.Visible = true;
+                Bar2.SetPosition(new Vector2(852f,92f));
+                Bar2.SetRotation((float)(3 * Math.PI / 4));
                 break;
         }
     }
-
-    private Cell EmptyCell()
-    {
-        List<Cell> emptyCells = AvailableCellsList();
-        int size = emptyCells.Count;
-        Random random = new Random();
-        Cell _cell = emptyCells[random.Next(size)];
-        return _cell;
-    }
-
-    private async void PCPlayCell()
-    {
-        Cell _cell = EmptyCell();
-        await ToSignal(GetTree().CreateTimer(_delay), "timeout");
-        OnCellClicked(_cell);
-
-    }
-
-    private List<Cell> AvailableCellsList()
-    {
-        List<Cell> availableCells = new List<Cell>();
-        foreach(Cell _cell in CellsArray)
-        {
-            if (!_cell.HasMark) availableCells.Add(_cell);
-        }
-
-        return availableCells;
-    }
-
 }
